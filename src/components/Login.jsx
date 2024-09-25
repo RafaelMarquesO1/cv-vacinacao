ea7343cd48d540e5033b250e4ccf8279d788bf39
RafaelMarquesO1 <rafaelmarquesm.0209@gmail.com>
2024-09-25 09:02:44 -0300

    att12

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,53 @@
-import "../styles/Login.jsx"
+import "../styles/Login.css"
+import ImagemDemonstrativa from '../assets/img/undraw_medicine_b-1-ol.svg'
 
 const Login = () => {
     return (
-        <div>
-            <h1>TESTE DA RAPAZIADA</h1>
-            <form className="forms">
-                <label>Nome:</label>
-                <input type="text" placeholder="Digite seu nome..." />
+        <div className="container">
 
-                <label>Email:</label>
-                <input type="text" placeholder="Digite seu email..." />
+            <div className="form-image">
+                <img src={ImagemDemonstrativa} alt="" />
+            </div>
 
-                <label>Senha:</label>
-                <input type="password" placeholder="Digite sua senha..." />
+        <div className="form">
+            <form action="#" method="get">
+                <div className="form-header">
+                    <h1>Tela de Login Administrativa</h1>
+                    
+                    <div className="login-button">
+                        <button><a href="#">Voltar para a home</a></button>
+                    </div>
+           
+                </div>
 
-                <label>Confirmar Senha:</label>
-                <input type="password" placeholder="Confirme sua senha..." />
+                <div className="input-group">
+                    <div className="input-box">
+                        <label htmlFor="">CPF</label>
+                        <input type="text" id="cpf" name="cpf" placeholder="xxx.xxx.xxx-xx" required />
+                    </div>
+
+                    <div className="input-box">
+                        <label htmlFor="">Email</label>
+                        <input type="email" id="email" name="email" placeholder="Digite seu email" required />
+                    </div>
+
+                    <div className="input-box">
+                        <label htmlFor="">Senha</label>
+                        <input type="password" id="password" name="password" placeholder="Digite sua senha" required />
+                    </div>
+
+                    <div className="input-box">
+                        <label htmlFor="">Confirme sua senha</label>
+                        <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Digite sua senha" required />
+                    </div>
+                </div>
+
+                <div className="continue-button">
+                    <button><a href="#">Continuar</a></button>
+                </div>
             </form>
+        </div>
+
         </div>
     )
 }
