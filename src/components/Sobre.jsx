3740985bd9b29302941bbf31e5b5cebf548803cb
RafaelMarquesO1 <rafaelmarquesm.0209@gmail.com>
2024-10-13 22:25:17 -0300

    att23

---
+++ src/components/Sobre.jsx
@@ -0,0 +1,11 @@
+import '../styles/styles.css'
+
+function Sobre() {
+    return (
+        <div>
+            <p>Está é a página sobre</p>
+        </div>
+    )
+}
+
+export default Sobre;
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,32 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // ...
+  build: {
+    // ...
+    sourcemap: true,
+    chunkSizeWarningLimit: 1000,
+    assetsDir: 'assets',
+    minify: 'terser',
+    manifest: true,
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'chunks/[name].js',
+        entryFileNames: 'chunks/[name].js',
+        assetFileNames: 'chunks/[name].[ext]',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': './src',
+    },
+  },
+  server: {
+    // ...
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/javascript',
+    },
+  },
+});
