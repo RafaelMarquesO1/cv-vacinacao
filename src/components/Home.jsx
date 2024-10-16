750ae48e87a00f7a6f1c1f36b8d43579ef37d6ac
RafaelMarquesO1 <rafaelmarquesm.0209@gmail.com>
2024-10-16 00:01:07 -0300

    att24u

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import '../styles/styles.css'
+import '../styles/Home.css'
 
 function Home() {
     return (
-        <div>
-            Home
+        <div className='container-base'>
+            <h1>SIGA PARA AS PRÓXIMAS PÁGINAS!</h1>
+            <h2>Seja bem vindo!</h2>
         </div>
     )
 }
--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -1,11 +1,20 @@
-import '../styles/styles.css'
+import '../styles/styles.css';
+import '../styles/Sobre.css';  // Importando o CSS específico para a página
 
 function Sobre() {
     return (
-        <div>
-            <p>Está é a página sobre</p>
+        <div className='container-base'>
+            <h1>Sobre o Vacinici</h1>
+            <p>Esta é a página de administração do aplicativo Vacinici, onde você pode gerenciar:</p>
+            <ul>
+                <li>Cadastro de Funcionários</li>
+                <li>Cadastro de Campanhas de Vacinação</li>
+                <li>Cadastro de Vacinas</li>
+                <li>Cadastro de Pacientes</li>
+            </ul>
+            <p>O aplicativo foi desenvolvido para facilitar a gestão de vacinação, proporcionando uma experiência simples e eficiente.</p>
         </div>
-    )
+    );
 }
 
 export default Sobre;
